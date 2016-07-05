23c9f2dafe2f54481e253adb109da4daa7175e4a
Roy <592151648@qq.com>
2016-07-06 00:22:50 +0800

    js demo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.onload=function(){
+	var go = document.getElementById('go'),
+		box = document.getElementById('box');
+
+	eventUtil.addHandler(box,'click',function(e){
+		alert('我是box');
+	});
+
+	eventUtil.addHandler(go,'click',function(e){
+		e = eventUtil.getEvent(e);
+		alert(eventUtil.getElement(e));
+		eventUtil.preventDefault(e);
+		eventUtil.stopPropagation(e);
+	});
+
+};
